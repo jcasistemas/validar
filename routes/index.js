1fb0c6f1b099e6cd0229a762c959f947fd3f3b02
JCA Sistemas <bogotano39@hotmail.com>
2015-06-03 02:19:20 -0500

    Primera Pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+// Version 2 - Importar el controlador
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Bienvenido a Quiz !!! <br><h3><span style="color:red;">¿Escapado?</span><h3>' });
 });
+
+// Version 2 - Importar el controlador
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer', quizController.answer);
 
 module.exports = router;
