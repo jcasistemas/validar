e2b516899f3e6c5bb11b4a06c202183a90cdbb4a
JC <bogotano39@hotmail.com>
2015-06-06 00:35:07 -0500

    express-partials - layout.ejs

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// 6- Layouts
+var partials = require('express-partials');
+
 var routes = require('./routes/index');
 
 // Eliminado de Version 0
@@ -15,6 +18,9 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+//6- Layouts
+app.use(partials());
 
 // Descomentada para Version 1 o colocar Link Rel en la vista html
 // uncomment after placing your favicon in /public
