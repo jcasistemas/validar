9399c15a7d8f24d37de3be739f013110431a1f0a
JC <bogotano39@hotmail.com>
2015-06-09 01:57:19 -0500

    Despliegue de la BD en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,18 +1,22 @@
 // GET /quizes/pregunta
 var models = require('../models/models.js'); // Dirección del modelo
+
+
 exports.pregunta = function( req, res) {
-	models.Quiz.findAll().success(function(quiz) {
-    res.render('quizes/pregunta', { pregunta: quiz[0].pregunta});
+	models.Quiz.findAll().then(function(quiz) {
+      res.render('quizes/pregunta', { pregunta: quiz[0].pregunta } );
+	})
 };
 
 // GET /quizes/respuesta
 exports.respuesta = function( req, res) {
-	models.Quiz.findAll().success(function(quiz) {
-    if (req.query.respuesta === quiz[0].respuesta) {
-      res.render('quizes/respuesta', { respuesta: 'Correcto' });
-    } else {
-      res.render('quizes/respuesta', { respuesta: 'Incorrecto'});
-    }
+	models.Quiz.findAll().then(function(quiz) {
+	    if (req.query.respuesta === quiz[0].respuesta) {
+	      res.render('quizes/respuesta', { respuesta: 'Correcto' });
+	    } else {
+	      res.render('quizes/respuesta', { respuesta: 'Incorrecto'});
+	    }
+	})
 };
 
 //Get /quizez/creditos
--- models/models.js
+++ models/models.js
@@ -1,30 +1,51 @@
 // Define como se construye todo el modelo 
 var path = require('path');
 
+// Paso 8
+// Postgres DATABASE_URL = postgres://user:passwd@host:port/database
+// SQLite   DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6]||null);
+var user     = (url[2]||null);
+var pwd      = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect  = (url[1]||null);
+var port     = (url[5]||null);
+var host     = (url[4]||null);
+var storage  = process.env.DATABASE_STORAGE;
+
 // Cargar Modelo ORM
 var Sequelize = require('sequelize');
 
-// Usar BBDD SQLite:
-var sequelize = new Sequelize(null, null, null, 
-  {dialect: "sqlite", storage: "quiz.sqlite"} // Archivo donde se guardaran los datos. BD (/quiz.sqlite)
+// Paso 8
+// Usar BBDD SQLite o Postgres
+var sequelize = new Sequelize(DB_name, user, pwd, 
+  { dialect:  protocol,
+    protocol: protocol,
+    port:     port,
+    host:     host,
+    storage:  storage,  // solo SQLite (.env)
+    omitNull: true      // solo Postgres
+  }      
 );
 
-// Importar la definicion de la tabla Quiz que está en /models/quiz.js
-var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+// Importar definicion de la tabla Quiz
+var quiz_path = path.join(__dirname,'quiz');
+var Quiz = sequelize.import(quiz_path);
 
 exports.Quiz = Quiz; // exportar definición de tabla Quiz para usarla en otros lugares de la aplicacion
 
 // Paso 2b. Inicializar la BD
 
-// sequelize.sync() crea e inicializa tabla de preguntas en DB - Forma Nueva
-sequelize.sync().success(function() {
-  // success(..) ejecuta el manejador una vez creada la tabla
-  Quiz.count().success(function (count){ // No de elementos de la tabla
-    if(count === 0) {   // la tabla se inicializa solo si está vacía
+// sequelize.sync() inicializa tabla de preguntas en DB
+sequelize.sync().then(function() {
+  // then(..) ejecuta el manejador una vez creada la tabla
+  Quiz.count().then(function (count){
+      if(count === 0) {   // la tabla se inicializa solo si está vacía
       Quiz.create({ pregunta: 'Capital de Italia',
                     respuesta: 'Roma'
       	           })
-      .success(function(){console.log('Base de datos inicializada')});
+       .then(function(){console.log('Base de datos inicializada')});
     };
   });
 });
